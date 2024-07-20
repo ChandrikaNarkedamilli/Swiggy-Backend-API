85c296f66218054ddde38a6c5ba9a2d5b0767d30
ChandrikaNarkedamilli <chandrikanarkedamilli07@gmail.com>
2024-07-20 13:07:17 +0530

    updated origin

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'https://swiggy-vendor-dashboard.vercel.app',
-  'https://swiggy-clone-eta-mocha.vercel.app/'
+  'https://swiggy-clone-eta-mocha.vercel.app'
 ];
 
 // Configure CORS to allow requests from allowed origins
